8ec873d83b51651da4e9f3671e3f20819c6b5e87
AzimUlmasov <azimulmasov2@gmail.com>
2023-01-13 15:50:51 +0500

    05

--- file05.py
+++ file05.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+
+    ans = []
+    i = 0
+    for num in data:
+        if num.isdigit():
+            i += 1
+    ans.append(i)
+    ans.append(len(data)- i)
+    return ans
+
 # Read data from file
+f = open('txt_file/data05.txt', encoding='UTF-8')
+data = f.read()
+print(main(data))
