ca072ca5cb31bfca2028dfda1cab708199b0af47
AzimUlmasov <azimulmasov2@gmail.com>
2023-01-13 16:34:49 +0500

    10

--- file10.py
+++ file10.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    lst = data.split('\n')
+    ans = []
+    for i in lst:
+        ans.append(len(i))
+    return max(ans)
+# Read data from file
+f = open('txt_file/data10.txt', encoding='UTF-8')
+data = f.read()
+print(main(data))    
 
 # Read data from file
