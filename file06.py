954d33f0f15d0226f3957c931441dbde7fab6653
AzimUlmasov <azimulmasov2@gmail.com>
2023-01-13 15:59:06 +0500

    06

--- file06.py
+++ file06.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    lst = data.split('\n')
+    ans = []
+    for i in lst:
+        ans.append(len(i))
+    return ans
 # Read data from file
+f = open('txt_file/data06.txt', encoding='UTF-8')
+data = f.read()
+print(main(data))
